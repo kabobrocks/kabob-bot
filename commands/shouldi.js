620645b3ce53b60e1abc1d119678379cb0a83b15
kabobrocks <kabobrocks@gmail.com>
2018-02-14 00:51:47 -0800

    added shouldI command and added file sending

--- commands/shouldi.js
+++ commands/shouldi.js
@@ -2,9 +2,9 @@ exports.run = function(client, message, args) {
 	var request =  require('request');
 
 	request({
-		url: "https://dog.ceo/api/breeds/image/random",
+		url: "https://yesno.wtf/api/",
 		json: true
 	}, function (error, response, body) {
-		message.channel.sendFile(body['message']);
+		message.channel.sendFile(body['image']);
 	})
 };
