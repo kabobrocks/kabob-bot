b079eccff6768d3fe1a34567c6e71f714833dfb2
kabobrocks <kabobrocks@gmail.com>
2018-02-14 11:16:03 -0800

    added image functionality to wolfram

--- commands/wolfram.js
+++ commands/wolfram.js
@@ -6,11 +6,15 @@ exports.run = function(client, message, args) {
 	var argsearch = args.join('+');
 
 	request({
-		url: `https://api.wolframalpha.com/v2/query?input=${argsearch}&format=plaintext&output=JSON&appid=` + wolframapi,
+		url: `https://api.wolframalpha.com/v2/query?input=${argsearch}&format=image,plaintext&output=JSON&appid=` + wolframapi,
 		json: true
 	}, function (error, response, body) {
 		if (body['queryresult']['success'] != false) {
-			message.channel.send(body['queryresult']['pods'][1]['subpods'][0]['plaintext']);
+			if (body['queryresult']['pods'][1]['subpods'][0]['img']['src'] != null) {
+				message.channel.send(body['queryresult']['pods'][1]['subpods'][0]['img']['src']);
+			} else {
+				message.channel.send(body['queryresult']['pods'][1]['subpods'][0]['plaintext']);
+			}
 		} else {
 			message.channel.send('I could not find an answer to that. Sorry.');
 		}
