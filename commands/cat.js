651d6017ef326d9b6216b11a30ff18da0e29fd9a
kabobrocks <kabobrocks@gmail.com>
2018-03-18 00:43:52 -0700

    updated cat api url

--- commands/cat.js
+++ commands/cat.js
@@ -2,7 +2,7 @@ exports.run = function(client, message, args) {
 	var request =  require('request');
 
 	request({
-		url: "http://random.cat/meow.php",
+		url: "http://aws.random.cat/meow.php",
 		json: true
 	}, function (error, response, body) {
 		message.channel.send(body);
